571b8f355e766c533d54bb6922c049a755dc2d73
Loïc Herbelot <loic.herbelot@telecom-paristech.fr>
2017-10-19 11:57:56 +0200

    RI - TME3 - Start of evaluation

--- RI/evaluation.py
+++ RI/evaluation.py
@@ -1,194 +1,59 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-""" 
-@author: Loïc Herbelot
-"""
+import query
+import numpy as np
 
-import io
-import re
+class IRList():
+    """ Contains a query and the scores found for this query """
+    def __init__(self, query, scores):
+        self.query = query
+        self.scores = scores # List [(docId, score)]
+
+    def getQuery(self):
+        return self.query
+
+    def getScores(self):
+        return self.scores
 
 
-class Query():
-    def __init__(self, queryID, text,relevants=None):
+class EvalMeasure:
+    """ The abstract class for measure methods """
+    def eval(irlist):
+        """:param irlist: The IRList object that contains the query and
+                          the docs found for that query"""
+
+        raise NotImplementedError("Abstract method")
+
+
+class PrecisionRecallMeasure(EvalMeasure):
+
+    def getRelevantResults(irlist):
+        # The relevant results of the query are the ones that have
+        # a score higher than minimum:
+        minScore = min([score for (docId, score) in irlist.getScores()])
+        relevantResults = [docId for (docId, score) in irlist.getScores()
+                          if score > minScore]
+
+    def precisionAt(i, irlist):
+        """ Return the number of real relevant results found, divided by i
         """
-        :param queryID:
-        :param text:
-        :param relevants: Dictionary {id: {"subtheme":int, "score":float}}
-        """
-        self.id = queryID
-        self.text = text
-        self.relevants = relevants
+        # The true relevants results are the one in Query.relevants
+        trueRels = list(irlist.getQuery().getRelevants().keys())
+        # Relevant results we found in the query:
+        relevantFound = np.intersect1d(getRelevantResults(irlist),
+                                    trueRels)
+        return len(relevantFound)/i
 
-    def getID(self):
-        return self.id
-
-    def getText(self):
-        return self.text
-
-    def getRelevants(self):
-        return self.relevants
-
-    def setRelevants(self, rel):
-        self.relevants = rel
-
-    def __str__(self):
-        return "id="+self.id+"\ntxt="+self.text+"\nrelevances="+str(self.relevants)
-
-
-class QueryParser:
-    def __init__(self, qry, rel):
-        """ Constructor
-        """
-        # open the files and get their lengths
-        self.qryFile = open(qry, "r")
-        self.qryLen = self.qryFile.seek(0, io.SEEK_END)
-        self.qryFile.seek(0)
-
-        self.relFile = open(rel, "r")
-        self.relLen = self.relFile.seek(0, io.SEEK_END)
-        self.relFile.seek(0)
-    
-    def __del__(self):
-        if (self.qryFile is not None):
-            self.qryFile.close()
-        if (self.relFile is not None):
-            self.relFile.close()
-    
-    def nextQuery(self):
-        # Extract data between start and end tags
-        # Call the abstract method "parseData" to construct a Query object
-        # Call the abstract method "extractRelevance(queryId)" 
-        # to extract the relevant docs and get a dictionary
-        # Add this dictionary to the Query object
-        raise NotImplementedError
-
-    def getQuery(self, queryText, queryRel):
-        raise NotImplementedError
-
-
-class QueryParserCACM(QueryParser):
-    def __init__(self, qryFile, relFile):
-        """ Constructor"""
-        self.begin = ".I"
-        super().__init__(qryFile, relFile)
-
-
-    def parseRelevance(self, data):
-        """ Parse the raw relevances in text to a dictionary
-        :return: A dictionary {id(int): {"subtheme":int, "score":float"}} 
-        """
-        dic = {}
-        for line in data.split('\n'):
-            if len(line) >1:
-                search = re.search("^\s*(\d+)\s*(\d+)\s*(\d+)\s*(\d+)\s*$", line)
-                qryId, docId, subtheme, score = search.groups()
-                dic[int(docId)] = {"subtheme":int(subtheme),
-                                   "score":float(score)}
-        return dic
-
-    def parseQuery(self, data):
-        """ Parse the raw query data into a Query object
-        .I [id]
-        .W
-         [text]
-        :return: a Query object"""
-        idSearch = re.search(".I (\d+)", data)
-        if idSearch is None:
-            print("Error, can't retrieve the ID of the query in '"+data+"'")
-        queryId = idSearch.group(1)
-
-        questionSearch = re.search(".W([\s\S]*?)\.[A-Z]", data)
-        if questionSearch is None:
-            print("Error, can't retrieve the question of the query in '"+data+"'")
-        question = questionSearch.group(1)
-
-        return Query(queryId, question)
+    def recallAt(i, irlist):
+        firstRelevantResults = getRelevantResults(irlist)[:i]
+        # The true relevants results are the one in Query.relevants
+        trueRels = list(irlist.getQuery().getRelevants().keys())
+        relevantFound = np.intersect1d(firstRelevantResults, trueRels)
+        return len(relevantFound) / len(trueRels)
 
 
 
-    def extractRelevance(self, qryId):
-        """ Extract the data from the .rel file"""
-
-        def startsNumber(s, n):
-            """Returns whether s starts with n (string)"""
-            return re.match("^0*(\d*)", s).group(1) == n
-
-        line = self.relFile.readline()
-        while (self.relFile.tell() < self.relLen and 
-               not startsNumber(line, qryId)):
-#            print(">", line, end='')
-            line = self.relFile.readline()
-        # if line is None, we have reached the end of the file and 
-        # we did not see any start tag.
-        if self.relFile.tell() == self.relLen:
-            return None
-#        print("Found the start:", line)
-        queryData = line
-
-#        print("Reading the relevances")
-        cursor = self.relFile.tell()
-        line = self.relFile.readline()
-        while (self.relFile.tell() < self.relLen and 
-               startsNumber(line, qryId)) :
-            queryData += line
-#            print(">", line, end='')
-            cursor = self.relFile.tell()
-            line = self.relFile.readline()
-
-        # Reposition the cursor before the new start tag
-        self.relFile.seek(cursor)
-        return queryData
-
-
-    def extractDataFromQry(self):
-        """ Return the data extracted and reposition the cursor in the qry file
-        :return: data
-        """     
-
-        # Read until the first start tag
-        line = self.qryFile.readline()
-        while (self.qryFile.tell() < self.qryLen and 
-               self.begin not in line) :
-#            print(">", line, end='')
-            line = self.qryFile.readline()
-        # if line is None, we have reached the end of the file and 
-        # we did not see any start tag.
-        if self.qryFile.tell() == self.qryLen:
-            return None
-#        print("Found the start:", line)
-#        print("Retrieve relevances")
-        qryId = re.search("0*(\d+)", line).group(1)
-        rels = self.extractRelevance(qryId)
-        queryData = line
-
-        # Read line by line until the end tag or a new start tag.
-#        print("Reading the data query")
-        cursor = self.qryFile.tell()
-        line = self.qryFile.readline()
-        while (self.qryFile.tell() < self.qryLen and 
-               self.begin not in line):
-#            print(">", line, end='')        
-            queryData += line
-            cursor = self.qryFile.tell()
-            line = self.qryFile.readline()
-
-        # Reposition the cursor before the new start tag
-        self.qryFile.seek(cursor)
-
-        return queryData, rels
-
-    def nextQuery(self):
-        queryData, queryRels =  self.extractDataFromQry()
-        query = self.parseQuery(queryData)
-        query.setRelevants(self.parseRelevance(queryRels))
-        return query
-
-
-
-
-if __name__ == "__main__":
-    qp = QueryParserCACM("cacm/cacm.qry", "cacm/cacm.rel")
-    print("Query:",qp.nextQuery())
-    print("Query:",qp.nextQuery())
-    print("Query:",qp.nextQuery())
+    def eval(irlist):
+        return [(precisionAt(i, irlist), recallAt(i, irlist))
+                for i in range(1, len(irlist.getScores()))]
--- RI/main.py
+++ RI/main.py
@@ -5,60 +5,70 @@ import ParserCACM
 import TextRepresenter
 import indexation
 import modeles
-
+from query import QueryParserCACM
+import evaluation
 
 if __name__ == "__main__":
-    cacm_txt = "cacm/cacm.txt"
+    cacm_txt = "cacm_sample/cacm.txt"
 
     # Construct the index:
     idx = indexation.Index("cacm", "gendata/")
     stemmer = TextRepresenter.PorterStemmer()
     idx.indexation(cacm_txt, ParserCACM.ParserCACM(),
                    stemmer)
-                   
-    print("\n###### A bit of testing: ###### ")
-    print("Retrieve stems in doc 20:")
-    print(idx.getTfsForDoc(20))
-    print("""Should look like 
-    “Accelerating Convergence of Iterative Processes
-A technique is discussed which, when applied
-to an iterative procedure for the solution of
-an equation, accelerates the rate of convergence if
-the iteration converges and induces convergence if
-the iteration diverges.  An illustrative example is given.”""")
 
-    print("\nRetrieve docs that contains 'iter':")
-    print(idx.getTfsForStem("iter"))
-    print("Should include doc 20") 
-    
+    print("\n###### A bit of testing: ###### ")
+    print("Retrieve stems in doc 46:")
+    print(idx.getTfsForDoc(46))
+    print("""Should look like
+    “Multiprogramming STRETCH: Feasibility Considerations
+    .W
+    The tendency towards increased parallelism in
+    computers is noted.  Exploitation of this parallelism
+    presents a number of new problems in machine design
+    and in programming systems.”""")
+
+    print("\nRetrieve docs that contains 'logic':")
+    print(idx.getTfsForStem("logic"))
+    print("Should include doc 63 and 46")
+
     print("\n###### Testing BinaryWeighter: ###### ")
     bw = modeles.BinaryWeighter(idx)
-    print("bw.getDocWeightsForDoc(20):", bw.getDocWeightsForDoc(20))
-    print("\nbw.getDocWeightsForStem(\"iter\"):", bw.getDocWeightsForStem("iter"))
-    query = stemmer.getTextRepresentation("Convergence of Iterative Processes")
+    print("bw.getDocWeightsForDoc(46):", bw.getDocWeightsForDoc(46))
+    print("\nbw.getDocWeightsForStem(\"logic\"):", bw.getDocWeightsForStem("logic"))
+    query = stemmer.getTextRepresentation("parallelism in programming")
     print("\nbw.getWeightsForQuery(query):", bw.getWeightsForQuery(query))
-    
+
 
     print("\n###### Testing Vectoriel with BinaryWeighter: ###### ")
     vect = modeles.Vectoriel(idx, bw)
     print("Top 10 document for the query:")
-    print(vect.getRanking(query)[-10:])
-    
+    print(vect.getRanking(query)[:10])
+
     print("\n###### Testing BinaryWeighter: ###### ")
     tfidfWeighter = modeles.TfidfWeighter(idx)
-    print("tfidfWeighter.getDocWeightsForDoc(20):", 
-        tfidfWeighter.getDocWeightsForDoc(20))
-    print("\ntfidfWeighter.getDocWeightsForStem(\"iter\"):", 
-        tfidfWeighter.getDocWeightsForStem("iter"))
-    query = stemmer.getTextRepresentation("Convergence of Iterative Processes")
-    print("\ntfidfWeighter.getWeightsForQuery(query):", 
-        tfidfWeighter.getWeightsForQuery(query))
-    
+    print("tfidfWeighter.getDocWeightsForDoc(46):",
+        tfidfWeighter.getDocWeightsForDoc(46))
+    print("\ntfidfWeighter.getDocWeightsForStem(\"logic\"):",
+        tfidfWeighter.getDocWeightsForStem("logic"))
+    q = stemmer.getTextRepresentation("parallelism in programming")
+    print("\ntfidfWeighter.getWeightsForQuery(query):",
+        tfidfWeighter.getWeightsForQuery(q))
+
 
     print("\n###### Testing Vectoriel with TfidfWeighter: ###### ")
     vect = modeles.Vectoriel(idx, tfidfWeighter)
     print("Top 10 document for the query:")
-    print(vect.getRanking(query)[-10:])
-    
-          
-    
+    print(vect.getRanking(q)[:10])
+
+    print("\n###### Testing QueryParserCACM: ###### ")
+    qp = QueryParserCACM("cacm_sample/cacm.qry", "cacm_sample/cacm.rel")
+    query = qp.nextQuery()
+    print("Display found queries:")
+    while query is not None:
+        print(query)
+        print(20*'-')
+        query = qp.nextQuery()
+
+    print("\n###### Testing evaluation.PrecisionRecallMeasure: ###### ")
+    # irlist = evaluation.IRList()
